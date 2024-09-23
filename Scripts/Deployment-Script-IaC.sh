17d447aef5e85ea29e386fa5e4ecf18999e8d2e7
EliasDeHondt <Elias.dehondt@outlook.com>
2024-09-23 11:08:19 +0200

    Refactor deployment script and add command to copy test data to pod

--- Scripts/Deployment-Script-IaC.sh
+++ Scripts/Deployment-Script-IaC.sh
@@ -20,32 +20,32 @@ max_nodes=5
 
 # Functie: Error afhandeling.
 function error_exit() {
-  echo -e "\n*\n* ${red}$1${reset}\n*\n* Exiting script.\n"
-  exit 1
+    echo -e "\n*\n* ${red}$1${reset}\n*\n* Exiting script.\n"
+    exit 1
 }
 
 # Functie: Succes afhandeling.
 function success() {
-  echo -e "\n*\n* ${green}$1${reset}\n*"
+    echo -e "\n*\n* ${green}$1${reset}\n*"
 }
 
 # Functie: Validate the external resources.
 function validate_external_resources() { # Step 0
-  if [ ! -f ./application.yaml ]; then error_exit "The application.yaml file is missing."; fi
-  if [ ! -f ./disney_bitconnect.mp4 ]; then error_exit "The disney_bitconnect.mp4 file is missing."; fi
+    if [ ! -f ./application.yaml ]; then error_exit "The application.yaml file is missing."; fi
+    if [ ! -f ./disney_bitconnect.mp4 ]; then error_exit "The disney_bitconnect.mp4 file is missing."; fi
 }
 
 # Functie: Enable the required APIs.
 function enable_apis() { # Step 1
-  gcloud services enable container.googleapis.com >./deployment-script.log 2>&1
-  local EXIT_CODE=$?
+    gcloud services enable container.googleapis.com >./deployment-script.log 2>&1
+    local EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then success "APIs enabled successfully."; else error_exit "Failed to enable the APIs."; fi
+    if [ $EXIT_CODE -eq 0 ]; then success "APIs enabled successfully."; else error_exit "Failed to enable the APIs."; fi
 }
 
 # Functie: Create the kubernetes cluster.
 function create_cluster() { # Step 2
-  gcloud container clusters create $cluster_name \
+    gcloud container clusters create $cluster_name \
     --region=$zone \
     --min-nodes=$min_nodes \
     --max-nodes=$max_nodes \
@@ -53,34 +53,34 @@ function create_cluster() { # Step 2
     --machine-type=n1-standard-4 \
     --disk-size=20GB \
     --enable-autoscaling >./deployment-script.log 2>&1
-  local EXIT_CODE=$?
+    local EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then success "Cluster created successfully."; else error_exit "Failed to create the cluster."; fi
+    if [ $EXIT_CODE -eq 0 ]; then success "Cluster created successfully."; else error_exit "Failed to create the cluster."; fi
 }
 
 # Functie: Get authentication credentials for the cluster.
 function get_credentials() { # Step 3
-  gcloud container clusters get-credentials $cluster_name --region=$zone >./deployment-script.log 2>&1
-  local EXIT_CODE=$?
+    gcloud container clusters get-credentials $cluster_name --region=$zone >./deployment-script.log 2>&1
+    local EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then success "Credentials retrieved successfully."; else error_exit "Failed to retrieve the credentials."; fi
+    if [ $EXIT_CODE -eq 0 ]; then success "Credentials retrieved successfully."; else error_exit "Failed to retrieve the credentials."; fi
 }
 
 # Functie: Deploy the application.
 function deploy_application() { # Step 4
-  kubectl apply -f ./apllication.yaml >./deployment-script.log 2>&1
-  local EXIT_CODE=$?
+    kubectl apply -f ./apllication.yaml >./deployment-script.log 2>&1
+    local EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then success "Application deployed successfully."; else error_exit "Failed to deploy the application."; fi
+    if [ $EXIT_CODE -eq 0 ]; then success "Application deployed successfully."; else error_exit "Failed to deploy the application."; fi
 }
 
 # Functie: Copy test data to volume.
 function copy_test_data() { # Step 5
-  local POD_NAME=$(kubectl get pods -l app=jellyfin -o jsonpath="{.items[0].metadata.name}")
-  kubectl cp ./disney_bitconnect.mp4 default/$POD_NAME:/media/disney_bitconnect.mp4
-  local EXIT_CODE=$?
+    local POD_NAME=$(kubectl get pods -l app=jellyfin -o jsonpath="{.items[0].metadata.name}")
+    kubectl cp ./disney_bitconnect.mp4 default/$POD_NAME:/media/disney_bitconnect.mp4
+    local EXIT_CODE=$?
 
-  if [ $EXIT_CODE -eq 0 ]; then success "Test data copied successfully."; else error_exit "Failed to copy the test data."; fi
+    if [ $EXIT_CODE -eq 0 ]; then success "Test data copied successfully."; else error_exit "Failed to copy the test data."; fi
 }
 
 # Functie: Set up SSL certificates for domain (For the load balancer external IP).
@@ -90,14 +90,13 @@ function setup_ssl_certificates() { # Step 6
 
 # Start of the script.
 function main() {
-  validate_external_resources # Step 0
-  enable_apis                 # Step 1
-  create_cluster              # Step 2
-  get_credentials             # Step 3
-  deploy_application          # Step 4
-  copy_test_data              # Step 5
-  setup_ssl_certificates      # Step 6
+    validate_external_resources # Step 0
+    enable_apis                 # Step 1
+    create_cluster              # Step 2
+    get_credentials             # Step 3
+    deploy_application          # Step 4
+    copy_test_data              # Step 5
+    setup_ssl_certificates      # Step 6
 }
 
 main # Start the script.
-
